d7f57640151214966211598f7c9168a839d05cb7
R.A. Porter <richard.porter@connexta.com>
2018-03-28 14:40:01 -0700

    Adds specific subclasses of the SAMLSpecRefMessage and includes doc names and URLs for the doc references.
    Also fixes the code trimming

--- library/src/main/kotlin/org/codice/compliance/SAMLSpecRefMessage.kt
+++ library/src/main/kotlin/org/codice/compliance/SAMLSpecRefMessage.kt
@@ -12,11 +12,21 @@
  * <http://www.gnu.org/licenses/lgpl.html>.
  */
 @file:Suppress("ClassNaming")
+
 package org.codice.compliance
 
+import java.net.URI
 import java.util.ResourceBundle
 
-sealed class SAMLSpecRefMessage {
+sealed class SAMLSpecRefMessage(docRefKey: String, docUriKey: String) {
+    val docRef: String
+    val docUri: URI
+
+    init {
+        docRef = bundle.getString(docRefKey)
+        docUri = URI(bundle.getString(docUriKey))
+    }
+
     companion object {
         private val bundle = ResourceBundle.getBundle("SAMLSpecRefMessage")
     }
@@ -30,151 +40,158 @@ sealed class SAMLSpecRefMessage {
     }
 }
 
+sealed class SAMLProfileRefMessage : SAMLSpecRefMessage("SAMLProfile.doc", "SAMLProfile.uri")
+sealed class SAMLCoreRefMessage : SAMLSpecRefMessage("SAMLCore.doc", "SAMLCore.uri")
+sealed class SAMLBindingRefMessage : SAMLSpecRefMessage("SAMLBinding.doc", "SAMLBinding.uri")
+sealed class XmlDatatypeRefMessage : SAMLSpecRefMessage("XMLDatatype.doc", "XMLDatatype.uri")
+sealed class XmlSigRefMessage : SAMLSpecRefMessage("XMLSig.doc", "XMLSig.uri")
+
 //-----------------
 // PROFILES
 //-----------------
-object SAMLProfiles_3_1_a : SAMLSpecRefMessage()
-object SAMLProfiles_3_1_b : SAMLSpecRefMessage()
-object SAMLProfiles_3_1_c : SAMLSpecRefMessage()
+object SAMLProfiles_3_1_a : SAMLProfileRefMessage()
 
-object SAMLProfiles_4_1_4_2_a : SAMLSpecRefMessage()
-object SAMLProfiles_4_1_4_2_b : SAMLSpecRefMessage()
-object SAMLProfiles_4_1_4_2_c : SAMLSpecRefMessage()
-object SAMLProfiles_4_1_4_2_d : SAMLSpecRefMessage()
-// object SAMLProfiles_4_1_4_2e : SAMLSpecRefMessage()
-object SAMLProfiles_4_1_4_2_f : SAMLSpecRefMessage()
+object SAMLProfiles_3_1_b : SAMLProfileRefMessage()
+object SAMLProfiles_3_1_c : SAMLProfileRefMessage()
 
-object SAMLProfiles_4_1_4_2_g : SAMLSpecRefMessage()
-object SAMLProfiles_4_1_4_2_h : SAMLSpecRefMessage()
-object SAMLProfiles_4_1_4_2_i : SAMLSpecRefMessage()
-object SAMLProfiles_4_1_4_2_j : SAMLSpecRefMessage()
-object SAMLProfiles_4_1_4_2_k : SAMLSpecRefMessage()
-object SAMLProfiles_4_1_4_5 : SAMLSpecRefMessage()
+object SAMLProfiles_4_1_4_2_a : SAMLProfileRefMessage()
+object SAMLProfiles_4_1_4_2_b : SAMLProfileRefMessage()
+object SAMLProfiles_4_1_4_2_c : SAMLProfileRefMessage()
+object SAMLProfiles_4_1_4_2_d : SAMLProfileRefMessage()
+// object SAMLProfiles_4_1_4_2e : SAMLProfileRefMessage()
+object SAMLProfiles_4_1_4_2_f : SAMLProfileRefMessage()
+
+object SAMLProfiles_4_1_4_2_g : SAMLProfileRefMessage()
+object SAMLProfiles_4_1_4_2_h : SAMLProfileRefMessage()
+object SAMLProfiles_4_1_4_2_i : SAMLProfileRefMessage()
+object SAMLProfiles_4_1_4_2_j : SAMLProfileRefMessage()
+object SAMLProfiles_4_1_4_2_k : SAMLProfileRefMessage()
+object SAMLProfiles_4_1_4_5 : SAMLProfileRefMessage()
 
 //-----------------
 // CORE
 //-----------------
-object SAMLCore_1_3_1_a : SAMLSpecRefMessage()
+object SAMLCore_1_3_1_a : SAMLCoreRefMessage()
 
-object SAMLCore_1_3_2_a : SAMLSpecRefMessage()
-object SAMLCore_1_3_3 : SAMLSpecRefMessage()
-object SAMLCore_1_3_4 : SAMLSpecRefMessage()
+object SAMLCore_1_3_2_a : SAMLCoreRefMessage()
+object SAMLCore_1_3_3 : SAMLCoreRefMessage()
+object SAMLCore_1_3_4 : SAMLCoreRefMessage()
 
-object SAMLCore_2_2_3_a : SAMLSpecRefMessage()
-object SAMLCore_2_2_3_b : SAMLSpecRefMessage()
+object SAMLCore_2_2_3_a : SAMLCoreRefMessage()
+object SAMLCore_2_2_3_b : SAMLCoreRefMessage()
 
-object SAMLCore_2_2_4_a : SAMLSpecRefMessage()
-// todo object SAMLCore_2_2_4_b : SAMLSpecRefMessage()
-// todo object SAMLCore_2_2_4_c : SAMLSpecRefMessage()
+object SAMLCore_2_2_4_a : SAMLCoreRefMessage()
+// todo object SAMLCore_2_2_4_b : SAMLCoreRefMessage()
+// todo object SAMLCore_2_2_4_c : SAMLCoreRefMessage()
 
-object SAMLCore_2_3_3_a : SAMLSpecRefMessage()
-object SAMLCore_2_3_3_b : SAMLSpecRefMessage()
-object SAMLCore_2_3_3_c : SAMLSpecRefMessage()
-object SAMLCore_2_3_4_a : SAMLSpecRefMessage()
-// todo object SAMLCore_2_3_4_b : SAMLSpecRefMessage()
+object SAMLCore_2_3_3_a : SAMLCoreRefMessage()
+object SAMLCore_2_3_3_b : SAMLCoreRefMessage()
+object SAMLCore_2_3_3_c : SAMLCoreRefMessage()
+object SAMLCore_2_3_4_a : SAMLCoreRefMessage()
+// todo object SAMLCore_2_3_4_b : SAMLCoreRefMessage()
 
-// todo object SAMLCore_2_4_1_2_b : SAMLSpecRefMessage()
+// todo object SAMLCore_2_4_1_2_b : SAMLCoreRefMessage()
 
-object SAMLCore_2_4_1_2_b : SAMLSpecRefMessage()
-object SAMLCore_2_4_1_3 : SAMLSpecRefMessage()
+object SAMLCore_2_4_1_2_b : SAMLCoreRefMessage()
+object SAMLCore_2_4_1_3 : SAMLCoreRefMessage()
 
-object SAMLCore_2_5_1_a : SAMLSpecRefMessage()
-object SAMLCore_2_5_1_b : SAMLSpecRefMessage()
-object SAMLCore_2_5_1_c : SAMLSpecRefMessage()
-object SAMLCore_2_5_1_2 : SAMLSpecRefMessage()
-object SAMLCore_2_5_1_5 : SAMLSpecRefMessage()
-object SAMLCore_2_5_1_6_a : SAMLSpecRefMessage()
-object SAMLCore_2_5_1_6_b : SAMLSpecRefMessage()
+object SAMLCore_2_5_1_a : SAMLCoreRefMessage()
+object SAMLCore_2_5_1_b : SAMLCoreRefMessage()
+object SAMLCore_2_5_1_c : SAMLCoreRefMessage()
+object SAMLCore_2_5_1_2 : SAMLCoreRefMessage()
+object SAMLCore_2_5_1_5 : SAMLCoreRefMessage()
+object SAMLCore_2_5_1_6_a : SAMLCoreRefMessage()
+object SAMLCore_2_5_1_6_b : SAMLCoreRefMessage()
 
-object SAMLCore_2_7_2 : SAMLSpecRefMessage()
-object SAMLCore_2_7_3 : SAMLSpecRefMessage()
-object SAMLCore_2_7_3_1_1 : SAMLSpecRefMessage()
-object SAMLCore_2_7_3_2_a : SAMLSpecRefMessage()
-// todo object SAMLCore_2_7_3_2_b : SAMLSpecRefMessage()
+object SAMLCore_2_7_2 : SAMLCoreRefMessage()
+object SAMLCore_2_7_3 : SAMLCoreRefMessage()
+object SAMLCore_2_7_3_1_1 : SAMLCoreRefMessage()
+object SAMLCore_2_7_3_2_a : SAMLCoreRefMessage()
+// todo object SAMLCore_2_7_3_2_b : SAMLCoreRefMessage()
 
-object SAMLCore_2_7_4 : SAMLSpecRefMessage()
+object SAMLCore_2_7_4 : SAMLCoreRefMessage()
 
-object SAMLCore_3_2_1_a : SAMLSpecRefMessage()
-object SAMLCore_3_2_1_b : SAMLSpecRefMessage()
-object SAMLCore_3_2_1_c : SAMLSpecRefMessage()
-object SAMLCore_3_2_2_a : SAMLSpecRefMessage()
-object SAMLCore_3_2_2_b : SAMLSpecRefMessage()
-object SAMLCore_3_2_2_c : SAMLSpecRefMessage()
-object SAMLCore_3_2_2_d : SAMLSpecRefMessage()
-object SAMLCore_3_2_2_e : SAMLSpecRefMessage()
-object SAMLCore_3_2_2_2 : SAMLSpecRefMessage()
+object SAMLCore_3_2_1_a : SAMLCoreRefMessage()
+object SAMLCore_3_2_1_b : SAMLCoreRefMessage()
+object SAMLCore_3_2_1_c : SAMLCoreRefMessage()
+object SAMLCore_3_2_2_a : SAMLCoreRefMessage()
+object SAMLCore_3_2_2_b : SAMLCoreRefMessage()
+object SAMLCore_3_2_2_c : SAMLCoreRefMessage()
+object SAMLCore_3_2_2_d : SAMLCoreRefMessage()
+object SAMLCore_3_2_2_e : SAMLCoreRefMessage()
+object SAMLCore_3_2_2_2 : SAMLCoreRefMessage()
 
-object SAMLCore_3_3_2_2_a : SAMLSpecRefMessage()
-object SAMLCore_3_3_2_2_b : SAMLSpecRefMessage()
-object SAMLCore_3_3_2_3 : SAMLSpecRefMessage()
+object SAMLCore_3_3_2_2_a : SAMLCoreRefMessage()
+object SAMLCore_3_3_2_2_b : SAMLCoreRefMessage()
+object SAMLCore_3_3_2_3 : SAMLCoreRefMessage()
 
-object SAMLCore_3_4 : SAMLSpecRefMessage()
-// todo object SAMLCore_3_4_1_1_a : SAMLSpecRefMessage()
+object SAMLCore_3_4 : SAMLCoreRefMessage()
+// todo object SAMLCore_3_4_1_1_a : SAMLCoreRefMessage()
 
-object SAMLCore_3_7_1 : SAMLSpecRefMessage()
+object SAMLCore_3_7_1 : SAMLCoreRefMessage()
 
-object SAMLCore_5_4_1 : SAMLSpecRefMessage()
-object SAMLCore_5_4_2_a : SAMLSpecRefMessage()
-object SAMLCore_5_4_2_b : SAMLSpecRefMessage()
-object SAMLCore_5_4_2_b1 : SAMLSpecRefMessage()
+object SAMLCore_5_4_1 : SAMLCoreRefMessage()
+object SAMLCore_5_4_2_a : SAMLCoreRefMessage()
+object SAMLCore_5_4_2_b : SAMLCoreRefMessage()
+object SAMLCore_5_4_2_b1 : SAMLCoreRefMessage()
 
-// todo object SAMLCore_6_1_a : SAMLSpecRefMessage()
-object SAMLCore_6_1_b : SAMLSpecRefMessage()
+// todo object SAMLCore_6_1_a : SAMLCoreRefMessage()
+object SAMLCore_6_1_b : SAMLCoreRefMessage()
 
 //-----------------
 // BINDINGS
 //-----------------
 // todo object SAMLBindings_3_1_1_b : SAMLSpecRefMessage()
-object SAMLBindings_3_1_2_1 : SAMLSpecRefMessage()
+object SAMLBindings_3_1_2_1 : SAMLBindingRefMessage()
 
-object SAMLBindings_3_4_3_a : SAMLSpecRefMessage()
-object SAMLBindings_3_4_3_b1 : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_a : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1 : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_a : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_a1 : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_a2 : SAMLSpecRefMessage()
-// todo object SAMLBindings_3_4_4_1_b : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_b1 : SAMLSpecRefMessage()
+object SAMLBindings_3_4_3_a : SAMLBindingRefMessage()
+object SAMLBindings_3_4_3_b1 : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_a : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1 : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_a : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_a1 : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_a2 : SAMLBindingRefMessage()
+// todo object SAMLBindings_3_4_4_1_b : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_b1 : SAMLBindingRefMessage()
 
-object SAMLBindings_3_4_4_1_b2 : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_c1 : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_c2 : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_d1 : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_d2 : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_e : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_f1 : SAMLSpecRefMessage()
-object SAMLBindings_3_4_4_1_f2 : SAMLSpecRefMessage()
-// todo object SAMLBindings_3_4_4_1_g : SAMLSpecRefMessage()
-object SAMLBindings_3_4_5_2_a1 : SAMLSpecRefMessage()
-// todo object SAMLBindings_3_4_5_2_a2 : SAMLSpecRefMessage()
+object SAMLBindings_3_4_4_1_b2 : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_c1 : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_c2 : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_d1 : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_d2 : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_e : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_f1 : SAMLBindingRefMessage()
+object SAMLBindings_3_4_4_1_f2 : SAMLBindingRefMessage()
+// todo object SAMLBindings_3_4_4_1_g : SAMLBindingRefMessage()
+object SAMLBindings_3_4_5_2_a1 : SAMLBindingRefMessage()
+// todo object SAMLBindings_3_4_5_2_a2 : SAMLBindingRefMessage()
 
-object SAMLBindings_3_4_6_a : SAMLSpecRefMessage()
-object SAMLBindings_3_5_3_a : SAMLSpecRefMessage()
-object SAMLBindings_3_5_3_b : SAMLSpecRefMessage()
-object SAMLBindings_3_5_4_a : SAMLSpecRefMessage()
-object SAMLBindings_3_5_4_a1 : SAMLSpecRefMessage()
-object SAMLBindings_3_5_4_a2 : SAMLSpecRefMessage()
-object SAMLBindings_3_5_4_b : SAMLSpecRefMessage()
-object SAMLBindings_3_5_4_b1 : SAMLSpecRefMessage()
-object SAMLBindings_3_5_4_c : SAMLSpecRefMessage()
-object SAMLBindings_3_5_4_d : SAMLSpecRefMessage()
-object SAMLBindings_3_5_4_d1 : SAMLSpecRefMessage()
-object SAMLBindings_3_5_4_d2 : SAMLSpecRefMessage()
-object SAMLBindings_3_5_5_2_a : SAMLSpecRefMessage()
-object SAMLBindings_3_5_6_a : SAMLSpecRefMessage()
+object SAMLBindings_3_4_6_a : SAMLBindingRefMessage()
+object SAMLBindings_3_5_3_a : SAMLBindingRefMessage()
+object SAMLBindings_3_5_3_b : SAMLBindingRefMessage()
+object SAMLBindings_3_5_4_a : SAMLBindingRefMessage()
+object SAMLBindings_3_5_4_a1 : SAMLBindingRefMessage()
+object SAMLBindings_3_5_4_a2 : SAMLBindingRefMessage()
+object SAMLBindings_3_5_4_b : SAMLBindingRefMessage()
+object SAMLBindings_3_5_4_b1 : SAMLBindingRefMessage()
+object SAMLBindings_3_5_4_c : SAMLBindingRefMessage()
+object SAMLBindings_3_5_4_d : SAMLBindingRefMessage()
+object SAMLBindings_3_5_4_d1 : SAMLBindingRefMessage()
+object SAMLBindings_3_5_4_d2 : SAMLBindingRefMessage()
+object SAMLBindings_3_5_5_2_a : SAMLBindingRefMessage()
+object SAMLBindings_3_5_6_a : SAMLBindingRefMessage()
 
 //-----------------
 // XML Datatype Schema
 //-----------------
 
-object XMLDatatypesSchema_3_2_7 : SAMLSpecRefMessage()
-object XMLDatatypesSchema_3_2_7_1_a : SAMLSpecRefMessage()
-object XMLDatatypesSchema_3_2_7_1_b : SAMLSpecRefMessage()
-object XMLDatatypesSchema_3_2_7_1_c : SAMLSpecRefMessage()
+object XMLDatatypesSchema_3_2_7 : XmlDatatypeRefMessage()
+object XMLDatatypesSchema_3_2_7_1_a : XmlDatatypeRefMessage()
+object XMLDatatypesSchema_3_2_7_1_b : XmlDatatypeRefMessage()
+object XMLDatatypesSchema_3_2_7_1_c : XmlDatatypeRefMessage()
 
 //-----------------
 // XML Signature Syntax and Processing
 //-----------------
 
-object XMLSignature_4_5 : SAMLSpecRefMessage()
+object XMLSignature_4_5 : XmlSigRefMessage()
